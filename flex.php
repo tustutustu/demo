fffb52cd912c22d8999c87282222b434646b851d
tustutustu <51863186+tustutustu@users.noreply.github.com>
2019-06-25 23:32:43 +0700

    Add files via upload

--- flex.php
+++ flex.php
@@ -6,123 +6,191 @@ $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' 
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 $jsonFlex = [
-    "type" => "flex",
-    "altText" => "Hello Flex Message",
-    "contents" => [
-      "type" => "bubble",
-      "direction" => "ltr",
-      "header" => [
-        "type" => "box",
-        "layout" => "vertical",
-        "contents" => [
-          [
-            "type" => "text",
-            "text" => "Purchase",
-            "size" => "lg",
-            "align" => "start",
-            "weight" => "bold",
-            "color" => "#009813"
+    "type"=> "flex",
+    "altText"=> "Flex Message",
+    "contents"=> [
+      "type"=> "carousel",
+      "contents"=> [
+        [
+          "type"=> "bubble",
+          "hero"=> [
+            "type"=> "image",
+            "url"=> "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
+            "size"=> "full",
+            "aspectRatio"=> "20:13",
+            "aspectMode"=> "cover"
           ],
-          [
-            "type" => "text",
-            "text" => "฿ 100.00",
-            "size" => "3xl",
-            "weight" => "bold",
-            "color" => "#000000"
-          ],
-          [
-            "type" => "text",
-            "text" => "Rabbit Line Pay",
-            "size" => "lg",
-            "weight" => "bold",
-            "color" => "#000000"
-          ],
-          [
-            "type" => "text",
-            "text" => "2019.02.14 21:47 (GMT+0700)",
-            "size" => "xs",
-            "color" => "#B2B2B2"
-          ],
-          [
-            "type" => "text",
-            "text" => "Payment complete.",
-            "margin" => "lg",
-            "size" => "lg",
-            "color" => "#000000"
-          ]
-        ]
-      ],
-      "body" => [
-        "type" => "box",
-        "layout" => "vertical",
-        "contents" => [
-          [
-            "type" => "separator",
-            "color" => "#C3C3C3"
-          ],
-          [
-            "type" => "box",
-            "layout" => "baseline",
-            "margin" => "lg",
-            "contents" => [
+          "body"=> [
+            "type"=> "box",
+            "layout"=> "vertical",
+            "spacing"=> "sm",
+            "contents"=> [
               [
-                "type" => "text",
-                "text" => "Merchant",
-                "align" => "start",
-                "color" => "#C3C3C3"
+                "type"=> "text",
+                "text"=> "Arm Chair, White",
+                "size"=> "xl",
+                "weight"=> "bold",
+                "wrap"=> true
               ],
               [
-                "type" => "text",
-                "text" => "BTS 01",
-                "align" => "end",
-                "color" => "#000000"
+                "type"=>"box",
+                "layout"=> "baseline",
+                "contents"=> [
+                  [
+                    "type"=> "text",
+                    "text"=> "$49",
+                    "flex"=> 0,
+                    "size"=> "xl",
+                    "weight": "bold",
+                    "wrap"=> true
+                  ],
+                  [
+                    "type"=> "text",
+                    "text"=> ".99",
+                    "flex"=> 0,
+                    "size"=> "sm",
+                    "weight"=> "bold",
+                    "wrap"=> true
+                  ]
+                ]
               ]
             ]
           ],
-          [
-            "type" => "box",
-            "layout" => "baseline",
-            "margin" => "lg",
-            "contents" => [
+          "footer"=> [
+            "type"=> "box",
+            "layout"=> "vertical",
+            "spacing"=> "sm",
+            "contents"=> [
               [
-                "type" => "text",
-                "text" => "New balance",
-                "color" => "#C3C3C3"
+                "type"=> "button",
+                "action"=> [
+                  "type"=> "uri",
+                  "label"=> "สมัครสมาชิก",
+                  "uri"=> "https://linecorp.com"
+                ],
+                "color"=> "#FD0A0A",
+                "style"=> "primary"
               ],
               [
-                "type" => "text",
-                "text" => "฿ 45.57",
-                "align" => "end"
+                "type"=> "button",
+                "action"=> [
+                  "type"=> "uri",
+                  "label"=> "เข้าเว็บ",
+                  "uri"=> "https://linecorp.com"
+                ],
+                "color"=>"#1BCA14",
+                "style"=> "primary"
+              ]
+            ]
+          ]
+        ],
+        [
+          "type"=> "bubble",
+          "hero"=> [
+            "type"=> "image",
+            "url"=> "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_6_carousel.png",
+            "size"=> "full",
+            "aspectRatio"=> "20:13",
+            "aspectMode"=> "cover"
+          ],
+          "body"=> [
+            "type"=> "box",
+            "layout"=> "vertical",
+            "spacing"=> "sm",
+            "contents"=> [
+              [
+                "type"=>"text",
+                "text"=>"Metal Desk Lamp",
+                "size"=> "xl",
+                "weight"=> "bold",
+                "wrap"=> true
+              ],
+              [
+                "type"=> "box",
+                "layout"=> "baseline",
+                "flex"=> 1,
+                "contents"=> [
+                  [
+                    "type"=> "text",
+                    "text"=> "$11",
+                    "flex"=> 0,
+                    "size"=>"xl",
+                    "weight"=> "bold",
+                    "wrap"=> true
+                  ],
+                  [
+                    "type"=> "text",
+                    "text"=> ".99",
+                    "flex"=> 0,
+                    "size"=> "sm",
+                    "weight"=>"bold",
+                    "wrap"=> true
+                  ]
+                ]
+              ],
+              [
+                "type"=> "text",
+                "text"=> "Temporarily out of stock",
+                "flex"=> 0,
+                "margin"=> "md",
+                "size"=> "xxs",
+                "color"=> "#FF5551",
+                "wrap"=> true
               ]
             ]
           ],
-          [
-            "type" => "separator",
-            "margin" => "lg",
-            "color" => "#C3C3C3"
+          "footer"=> [
+            "type"=> "box",
+            "layout"=> "vertical",
+            "spacing"=> "sm",
+            "contents"=> [
+              [
+                "type"=> "button",
+                "action"=> [
+                  "type"=> "uri",
+                  "label"=> "สมัครสมาชิก",
+                  "uri"=> "https://linecorp.com"
+                ],
+                "flex"=> 2,
+                "color"=> "#FD0A0A",
+                "style"=> "primary"
+              ],
+              [
+                "type"=> "button",
+                "action"=> [
+                  "type"=> "uri",
+                  "label"=> "เข้าเว็บ",
+                  "uri"=> "https://linecorp.com"
+                ],
+                "color"=> "#1BCA14",
+                "style"=> "primary"
+              ]
+            ]
           ]
-        ]
-      ],
-      "footer" => [
-        "type" => "box",
-        "layout" => "horizontal",
-        "contents" => [
-          [
-            "type" => "text",
-            "text" => "View Details",
-            "size" => "lg",
-            "align" => "start",
-            "color" => "#0084B6",
-            "action" => [
-              "type" => "uri",
-              "label" => "View Details",
-              "uri" => "https://google.co.th/"
+        ],
+        [
+          "type"=> "bubble",
+          "body"=> [
+            "type"=> "box",
+            "layout"=> "vertical",
+            "spacing"=> "sm",
+            "contents"=> [
+              [
+                "type"=> "button",
+                "action"=> [
+                  "type"=> "uri",
+                  "label"=> "ดูเพิ่ม",
+                  "uri"=> "https://linecorp.com"
+                ],
+                "flex"=> 1,
+                "gravity"=> "center"
+              ]
             ]
           ]
         ]
       ]
     ]
-  ];
+];
 if ( sizeof($request_array['events']) > 0 ) {
     foreach ($request_array['events'] as $event) {
         error_log(json_encode($event));
