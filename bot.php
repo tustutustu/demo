3bd8b25c71de1ded815daa0b131d8ab7435cc0a4
tustutustu <51863186+tustutustu@users.noreply.github.com>
2019-06-25 19:52:51 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -216,7 +216,7 @@
       curl_setopt($ch, CURLOPT_HEADER, false);
       curl_setopt($ch, CURLOPT_POST, true);
       curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $arrayPostData);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
       $result = curl_exec($ch);
