f98f02ee02866631084675065faf648cd84c44ce
tustutustu <51863186+tustutustu@users.noreply.github.com>
2019-06-26 20:45:00 +0700

    Add files via upload

--- LINEBot.php
+++ LINEBot.php
@@ -1,5 +1,5 @@
 <?php
-   $accessToken = "GXtc+040e4As0jFqmIWL/cI1cr48MZeXtwD2X6fh74Rw7Mk09LYByMWeBQZdiZRsBjmhV918UIFclBhJji87CThJbsvLez47hJA/lI0w4oy0lGeJ2uuA4SCQPiwO8rZnZrfcE9n68Q9L5Av3GNEWDwdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
+   $accessToken = "";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
    $content = file_get_contents('php://input');
    $arrayJson = json_decode($content, true);
    $arrayHeader = array();
@@ -10,194 +10,14 @@
    //รับ id ของผู้ใช้
    $id = $arrayJson['events'][0]['source']['userId'];
    #ตัวอย่าง Message Type "Text + Sticker"
-   $arrayPostData = [
-    "type" => "flex",
-    "altText" => "Flex Message",
-    "contents" => [
-      "type" => "carousel",
-      "contents" => [
-        [
-          "type" => "bubble",
-          "hero" => [
-            "type" => "image",
-            "url" => "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
-            "size" => "full",
-            "aspectRatio" => "20:13",
-            "aspectMode" => "cover"
-          ],
-          "body" => [
-            "type" => "box",
-            "layout" => "vertical",
-            "spacing" => "sm",
-            "contents" => [
-              [
-                "type" => "text",
-                "text" => "Arm Chair, White",
-                "size" => "xl",
-                "weight" => "bold",
-                "wrap" => true
-              ],
-              [
-                "type" =>"box",
-                "layout" => "baseline",
-                "contents" => [
-                  [
-                    "type" => "text",
-                    "text" => "$49",
-                    "flex" => 0,
-                    "size" => "xl",
-                    "weight" => "bold",
-                    "wrap" => true
-                  ],
-                  [
-                    "type" => "text",
-                    "text" => ".99",
-                    "flex" => 0,
-                    "size" => "sm",
-                    "weight" => "bold",
-                    "wrap" => true
-                  ]
-                ]
-              ]
-            ]
-          ],
-          "footer" => [
-            "type" => "box",
-            "layout" => "vertical",
-            "spacing" => "sm",
-            "contents" => [
-              [
-                "type" => "button",
-                "action" => [
-                  "type" => "uri",
-                  "label" => "สมัครสมาชิก",
-                  "uri" => "https://linecorp.com"
-                ],
-                "color" => "#FD0A0A",
-                "style" => "primary"
-              ],
-              [
-                "type" => "button",
-                "action" => [
-                  "type" => "uri",
-                  "label" => "เข้าเว็บ",
-                  "uri" => "https://linecorp.com"
-                ],
-                "color" =>"#1BCA14",
-                "style" => "primary"
-              ]
-            ]
-          ]
-        ],
-        [
-          "type" => "bubble",
-          "hero" => [
-            "type" => "image",
-            "url" => "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_6_carousel.png",
-            "size" => "full",
-            "aspectRatio" => "20:13",
-            "aspectMode" => "cover"
-          ],
-          "body" => [
-            "type" => "box",
-            "layout" => "vertical",
-            "spacing" => "sm",
-            "contents" => [
-              [
-                "type" =>"text",
-                "text" =>"Metal Desk Lamp",
-                "size" => "xl",
-                "weight" => "bold",
-                "wrap" => true
-              ],
-              [
-                "type" => "box",
-                "layout" => "baseline",
-                "flex" => 1,
-                "contents" => [
-                  [
-                    "type" => "text",
-                    "text" => "$11",
-                    "flex" => 0,
-                    "size" =>"xl",
-                    "weight" => "bold",
-                    "wrap" => true
-                  ],
-                  [
-                    "type" => "text",
-                    "text" => ".99",
-                    "flex" => 0,
-                    "size" => "sm",
-                    "weight" =>"bold",
-                    "wrap" => true
-                  ]
-                ]
-              ],
-              [
-                "type" => "text",
-                "text" => "Temporarily out of stock",
-                "flex" => 0,
-                "margin" => "md",
-                "size" => "xxs",
-                "color" => "#FF5551",
-                "wrap" => true
-              ]
-            ]
-          ],
-          "footer" => [
-            "type" => "box",
-            "layout" => "vertical",
-            "spacing" => "sm",
-            "contents" => [
-              [
-                "type" => "button",
-                "action" => [
-                  "type" => "uri",
-                  "label" => "สมัครสมาชิก",
-                  "uri" => "https://linecorp.com"
-                ],
-                "flex" => 2,
-                "color" => "#FD0A0A",
-                "style" => "primary"
-              ],
-              [
-                "type" => "button",
-                "action" => [
-                  "type" => "uri",
-                  "label" => "เข้าเว็บ",
-                  "uri" => "https://linecorp.com"
-                ],
-                "color" => "#1BCA14",
-                "style" => "primary"
-              ]
-            ]
-          ]
-        ],
-        [
-          "type" => "bubble",
-          "body" => [
-            "type" => "box",
-            "layout" => "vertical",
-            "spacing" => "sm",
-            "contents" => [
-              [
-                "type" => "button",
-                "action" => [
-                  "type" => "uri",
-                  "label" => "ดูเพิ่ม",
-                  "uri" => "https://linecorp.com"
-                ],
-                "flex" => 1,
-                "gravity" => "center"
-              ]
-            ]
-          ]
-        ]
-      ]
-    ]
-];
-$ThatTime ="20:40:00";
-if (time() == strtotime($ThatTime)) {
+   $ThatTime ="20:00:00";
+if (time() >= strtotime($ThatTime)) {
+      $arrayPostData['to'] = $id;
+      $arrayPostData['messages'][0]['type'] = "text";
+      $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+      $arrayPostData['messages'][1]['type'] = "sticker";
+      $arrayPostData['messages'][1]['packageId'] = "2";
+      $arrayPostData['messages'][1]['stickerId'] = "34";
       pushMsg($arrayHeader,$arrayPostData);
    }
    function pushMsg($arrayHeader,$arrayPostData){
